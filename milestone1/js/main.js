14cea990ab577ad6782cf13ca56adf844e438eaf
DanCiciarello <me@danieleciciarello.com>
2022-06-14 17:03:36 +0200

    Completed milestone1

--- milestone1/js/main.js
+++ milestone1/js/main.js
@@ -5,9 +5,23 @@ new Vue ({
     // App data
     data: {
         userChatList: contatti,
+        selectedChatIndex: 0,
+        selectedUserChatName: "Michele",
+        selectedUserChatImage: "../img/avatar_1.jpg"
     },
 
     // App methods
-    methods: {},
+    methods: {
+
+        // Selected chat item
+        selectedChat (index) {
+            // Al click, registro l'indice dell'elemento cliccato
+            this.selectedChatIndex = index;
+            // Cambio il nome e l'immagine della chat attiva
+            this.selectedUserChatName = this.userChatList[index].name;
+            this.selectedUserChatImage = "../img/avatar" + this.userChatList[index].avatar + ".jpg";
+        },
+
+    },
 
 });
