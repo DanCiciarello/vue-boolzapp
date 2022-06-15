9901a23e8298604032621e89b4cde8358ead52b4
DanCiciarello <me@danieleciciarello.com>
2022-06-15 22:43:10 +0200

    Completed milestone4

--- milestone4/js/main.js
+++ milestone4/js/main.js
@@ -10,11 +10,14 @@ new Vue ({
         selectedUserChatImage: "../img/avatar_1.jpg",
         selectedUserChat: "",
         newMessage: "",
+        searchInputText: "",
+        userChatListFiltered: "",
     },
 
     mounted () {
         this.selectedChatIndex = 0;
         this.selectedUserChat = this.userChatList[0].messages;
+        this.userChatListFiltered = this.userChatList;
     },
 
     // App methods
@@ -51,6 +54,12 @@ new Vue ({
             };
             // Pusho la risposta dopo 1 secondo
             setTimeout(() => this.selectedUserChat.push(newMessageObjReply), 1000);
+        },
+
+        // Filter the user list
+        filterUserList () {
+            // Filtro la lista utenti ad ogni lettera inserita
+            this.userChatListFiltered = this.userChatList.filter(user => user.name.toLowerCase().includes(this.searchInputText));
         },
 
     },
